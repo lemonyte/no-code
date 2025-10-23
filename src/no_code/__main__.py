bcda3ffcbc67c0e9e13ea7a36c4c10bcfd5caabe
lemonyte <49930425+lemonyte@users.noreply.github.com>
2025-10-23 15:28:33 +0200

    Fix extra trailing newline in command outputc

--- src/no_code/__main__.py
+++ src/no_code/__main__.py
@@ -6,12 +6,14 @@ from no_code.no import no_code, something
 
 def no_code_cmd() -> None:
     code = Path(sys.argv[1]).read_text(encoding="utf-8") if len(sys.argv) > 1 else sys.stdin.read()
-    print(no_code(code))
+    sys.stdout.write(no_code(code))
+    sys.stdout.flush()
 
 
 def yes_code_cmd() -> None:
     code = Path(sys.argv[1]).read_text(encoding="no-code") if len(sys.argv) > 1 else something(sys.stdin.read())
-    print(code.removeprefix("# coding: no\n"))
+    sys.stdout.write(code.removeprefix("# coding: no\n"))
+    sys.stdout.flush()
 
 
 if __name__ == "__main__":
